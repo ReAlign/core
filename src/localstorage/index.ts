37f05fd367dd448cac1fab469b00d84fddb10a76
realign <realign@yeah.net>
2020-08-11 00:20:45 +0800

    re:add ls

---
+++ src/localstorage/index.ts
@@ -0,0 +1,12 @@
+// import fetcher from './fetcher';
+// import get from './get';
+// import post from './post';
+
+// export {
+//   fetcher as fetch,
+//   get,
+//   post,
+// };
+
+// https://github.com/ReAlign/r-localstorage.git
+// https://github.com/ReAlign/LocalStorage [getSurplusCapacity]
